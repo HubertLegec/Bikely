b4ba3189b3077994c154eae52b36f968339e2c5c
Tomasz Massalski <tomasz.massalski@gmail.com>
2021-03-11 23:02:53 +0100

    Adds missing test, fix in updateBike

--- src/bikes/bikes.controller.spec.ts
+++ src/bikes/bikes.controller.spec.ts
@@ -6,10 +6,35 @@ import { BikesModule } from './bikes.module';
 import { testModuleWithInMemoryDb } from '../utils/test-utils';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { assert } from 'console';
+import { Model } from 'mongoose';
+import { Bike } from './bike.model';
+
+const bikeRequestCorrect: BikeRequest = {
+  type: BikeType.mtb,
+  isElectric: false,
+  frameSize: 20,
+};
+const bikeRequestIncorrectType = {
+  type: 'test',
+  isElectric: true,
+  frameSize: 20,
+};
+const bikeRequestIncorrectFrameSize = {
+  type: BikeType.city,
+  isElectric: false,
+  frameSize: 2,
+};
+
+const patchTypeAndFrameSizeRequest = {
+  type: 'city',
+  frameSize: 15,
+};
 
 describe('BikesController', () => {
   let app: INestApplication;
   let mongoServer: MongoMemoryServer;
+  let bikeModel: Model<Bike>;
+  let createdBikeId: string;
 
   beforeAll(async () => {
     const moduleWithDb = await testModuleWithInMemoryDb({
@@ -19,12 +44,55 @@ describe('BikesController', () => {
     const module = moduleWithDb.module;
     mongoServer = moduleWithDb.mongoServer;
 
+    bikeModel = module.get('BikeModel');
     app = module.createNestApplication();
     app.useGlobalPipes(new ValidationPipe());
     await app.init();
   });
 
-  describe(`POST addBike`, () => {
+  beforeEach(async () => {
+    const createdBike = await bikeModel.create(bikeRequestCorrect);
+    createdBikeId = createdBike.id;
+  });
+
+  afterEach(async () => {
+    await bikeModel.deleteMany({}).exec();
+  });
+
+  describe('GET /admin/bikes/:id', () => {
+    it('should return correct bike', (done) => {
+      const givenId = createdBikeId;
+      request(app.getHttpServer())
+        .get(`/admin/bikes/${givenId}`)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(HttpStatus.OK)
+        .then((res) => {
+          assert(res.body.message, {
+            _id: createdBikeId,
+            type: 'mtb',
+            isElectric: false,
+            frameSize: 20,
+            __v: 0,
+          });
+          done();
+        });
+    });
+    it('should return 404', (done) => {
+      const givenId = 'fakeId';
+      request(app.getHttpServer())
+        .get(`/admin/bikes/${givenId}`)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(HttpStatus.NOT_FOUND)
+        .then((res) => {
+          assert(res.body.message, [`Could not find bike with id: ${givenId}`]);
+          done();
+        });
+    });
+  });
+
+  describe(`POST /admin/bikes/`, () => {
     it(`Should return HttpStatus.CREATED`, (done) => {
       request(app.getHttpServer())
         .post('/admin/bikes')
@@ -57,24 +125,53 @@ describe('BikesController', () => {
     });
   });
 
+  describe('PATCH /admin/bikes/:id', () => {
+    it('should return bike with applied patch', (done) => {
+      const givenId = createdBikeId;
+      request(app.getHttpServer())
+        .patch(`/admin/bikes/${givenId}`)
+        .send(patchTypeAndFrameSizeRequest)
+        .set('Accept', 'application/json')
+        .expect(HttpStatus.OK)
+        .then(async () => {
+          const bikesCount = await bikeModel.countDocuments({}).exec();
+          expect(bikesCount).toEqual(1);
+          const bike = await bikeModel.findOne({ _id: givenId }).exec();
+          expect(bike.type).toEqual(BikeType.city);
+          expect(bike.isElectric).toEqual(false);
+          expect(bike.frameSize).toEqual(15);
+          done();
+        });
+    });
+  });
+
+  describe('DELETE /admin/bikes/:id', () => {
+    it('should remove bike with given ID', (done) => {
+      const idToDelete = createdBikeId;
+      request(app.getHttpServer())
+        .delete(`/admin/bikes/${idToDelete}`)
+        .expect(HttpStatus.OK)
+        .then(async () => {
+          const remainingAccounts = await bikeModel.find({}).exec();
+          expect(remainingAccounts).toHaveLength(0);
+          done();
+        });
+    });
+    it('should return 404', async (done) => {
+      const idToDelete = createdBikeId;
+      await bikeModel.deleteOne({ _id: idToDelete });
+      request(app.getHttpServer())
+        .delete(`/admin/bikes/${idToDelete}`)
+        .expect(HttpStatus.NOT_FOUND)
+        .then((res) => {
+          assert(res.body.message, [`Could not delete bike with id: ${idToDelete}`]);
+          done();
+        });
+    });
+  });
+
   afterAll(async () => {
     await app.close();
     await mongoServer.stop();
   });
-
-  const bikeRequestCorrect: BikeRequest = {
-    type: BikeType.mtb,
-    isElectric: false,
-    frameSize: 20,
-  };
-  const bikeRequestIncorrectType = {
-    type: 'test',
-    isElectric: true,
-    frameSize: 20,
-  };
-  const bikeRequestIncorrectFrameSize = {
-    type: BikeType.city,
-    isElectric: false,
-    frameSize: 2,
-  };
 });
