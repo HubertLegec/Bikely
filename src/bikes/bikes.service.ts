e2cba80bf11ce0e54043be539698723418774a16
tmassalski <tomasz.massalski@gmail.com>
2021-03-14 14:02:29 +0100

    Bike crud (#6)
    * Adds CRUD operations for bikes
    
    * formatting fix
    
    * Adds missing test, fix in updateBike
    
    * Adds optimistic concurrenct for BikeSchema
    
    * adds workflow for deploying to heroku (#5)
    
    * adds workflow for deploying to heroku
    
    * Admin api add bike (#4)
    
    * add new bike api
    
    * removes isFree attribute
    
    * add missing memory-pager
    
    * bike model change, basic bike dto validation
    
    * adds validation pipe and bikes controller tests
    
    * bikes controller tests refactoring
    
    * bikes controller test fix
    
    * mongoDb uri fix in test utils
    
    * adds Procfile, node engine, and heroku port
    
    * adds workflow for deploying to heroku
    
    * adds Procfile, node engine, and heroku port
    
    * Adds CRUD operations for bikes
    
    * formatting fix
    
    * Adds missing test, fix in updateBike
    
    * Adds optimistic concurrenct for BikeSchema

--- src/bikes/bikes.service.ts
+++ src/bikes/bikes.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { Bike } from './bike.model';
 import { BikeRequest } from './bikeRequest.dto';
+import { BikeUpdate } from './bikeUpdate.dto';
 
 @Injectable()
 export class BikesService {
@@ -17,5 +18,41 @@ export class BikesService {
     });
     const result = await newBike.save();
     return result.id;
+  }
+
+  async getBike(bikeId: string) {
+    const bike = await this.findBike(bikeId);
+    return bike;
+  }
+
+  async updateBike(bikeId: string, bikeUpdate: BikeUpdate) {
+    const bike = await this.findBike(bikeId);
+    if (bikeUpdate.type) {
+      bike.type = bikeUpdate.type;
+    }
+    if (bikeUpdate.isElectric !== undefined) {
+      bike.isElectric = bikeUpdate.isElectric;
+    }
+    if (bikeUpdate.frameSize) {
+      bike.frameSize = bikeUpdate.frameSize;
+    }
+    bike.save();
+  }
+
+  async deleteBike(bikeId: string) {
+    const deleteResult = await this.bikeModel.deleteOne({ _id: bikeId });
+    if (deleteResult.n === 0) {
+      throw new NotFoundException(`Could not delete bike with id: ${bikeId}`);
+    }
+  }
+
+  async findBike(bikeId: string) {
+    let bike: Bike;
+    try {
+      bike = await this.bikeModel.findById(bikeId);
+    } catch (error) {
+      throw new NotFoundException(`Could not find bike with id: ${bikeId}`);
+    }
+    return bike;
   }
 }
